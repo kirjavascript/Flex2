cfc6232aeab2d8e215c5c513fc9185ff0058455a
kirjavascript <snkenjoi@gmail.com>
2017-10-10 23:38:30 +0100

    flood fill bugfixes

--- modules/components/import/state.js
+++ modules/components/import/state.js
@@ -64,6 +64,7 @@ class ImportState {
         const { ctx, canvas } = this;
         const { width, height } = canvas;
         const buffer = ctx.getImageData(0, 0, width, height);
+        let t0 = performance.now();
 
         // get top left pixel
         const [r, g, b, a] = ctx.getImageData(0, 0, 1, 1).data;
@@ -96,11 +97,13 @@ class ImportState {
             }
         }
 
+        ctx.putImageData(buffer, 0, 0);
+
         // get bboxes
 
         function getXY(pos) {
             const x = (pos / 4) % width;
-            const y = (((firstPos / 4) - x) / width);
+            const y = (((pos / 4) - x) / width);
             return [x, y];
         }
 
@@ -110,7 +113,6 @@ class ImportState {
 
         function setPixel(pos) {
             buffer.data[pos+3] = 0;
-            // track x/y here
         }
 
         function checkMatch(pos) {
@@ -120,6 +122,7 @@ class ImportState {
         // get first pixel
         let firstPos = -1;
 
+
         for (let i = 0, j = 0; i < (width * height); i++, j+=4) {
             if (buffer.data[j+3] > 0x80) {
                 firstPos = j;
@@ -128,10 +131,11 @@ class ImportState {
         }
 
         // if no pixels found...
-        if (firstPos == -1) return;
+        if (firstPos == -1) return -1;
 
         let [x, y] = getXY(firstPos);
 
+        // flood fill from initial pixel
 
         let stack = [[x, y]];
 
@@ -149,7 +153,7 @@ class ImportState {
             reachLeft = false;
             reachRight = false;
 
-            if (stack.length > 100000) return;
+            if (stack.length > 1000000) return;
 
             while (y++ < height - 1 && checkMatch(pos)) {
                 setPixel(pos);
@@ -220,6 +224,10 @@ function nearestColor(line = 0) {
     };
 }
 
+// for fuzzyness - after removing, search again in and around the bbox
+// track min/max, first is min/max
+// once the search is complete, grab the bbox and unset the transparency again :)
+
 //start from top left
 //mappings dont have to be in a grid
 //https://github.com/sonicretro/SonLVL/blob/master/SpritePlotter.NET/Program.cs#L324
@@ -232,6 +240,28 @@ function nearestColor(line = 0) {
 // get bbox -> packing algorithm -> remove empty maps -> shrink where possible
 //
 // bbox: find first pixel -> flood fill non transparent -> trackx/y for bbox
+//
+// 22:58:15 <%snkenjoi> there's always the option to best guess and then allow the user to clean up afterwards -> yes to all
+// 22:58:28 <%snkenjoi> so, present each sprite for adjustment
+// 22:58:40 < flamewing> I think that would be a good compromise, yeah
+// 22:59:14 <%snkenjoi> because it sounds otherwise this is going to be a bit hellish :)
+//
+// 22:59:26 < flamewing> *If* the UI makes it very easy to change the resulting split
+//
+// 22:37:07 < flamewing> Minimum number of tiles, minimum number of sprite pieces, minimum number of sprite pieces per scanline, and so on
+//
+// 22:46:55 < flamewing> For example: starting by making tile rows that cover the image with minimal slack, and are hugging the top-left of the image as much as possible; then shift lines 1 pixel at a time, adding
+//                      tiles (and removing blank tiles) as needed, and trying to merge into sprites. Then shift the initial cover up by 1 pixel and add/remove tiles as needed; repeat. You never need to shift each
+//                      line by more than 7 pixels, and you don't need to shift the cover
+// 22:46:56 < flamewing> vertically by more than 7 pixels
+// 22:48:35 < flamewing> It is probably not fast, but you can probably put some bounds, and maybe refine by trying to keep similarly-sized rows of tiles in lock-step
+// 22:48:44 <%snkenjoi> the merging is the tricky part
+// 22:48:56 <%snkenjoi> I think?
+// 22:49:06 <%snkenjoi> actually maybe not
+// 22:49:10 >>  Techokami!Techokami@bdnk-8238933a.fios.verizon.net has joined: #SSRG
+// 22:49:28 < djohe> flamewing you got an IRC PM
+// 22:49:39 < flamewing> When doing the sliding, you can probably do a compressed bitmap version with a single bit per tile
+// 22:50:21 < flamewing> And maybe with a whole lot of analysis, maybe even do some large lookup tables
 
 const importState = new ImportState();
 export { importState };
