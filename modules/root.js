b42c1a44ab11829af6ec9a0d96d3dafafff62cf7
kirjavascript <snkenjoi@gmail.com>
2017-11-19 18:10:01 +0000

    version bump

--- modules/root.js
+++ modules/root.js
@@ -7,16 +7,14 @@ import './components/import';
 
 import Analytics from 'electron-google-analytics';
 
+(new Analytics('UA-109903721-1'))
+    .pageview('http://flex2.kirjava.xyz', '/', 'Main View')
+    .then((response) => { }).catch((err) => { });
+
 render(
     <Layout/>,
     document.body.appendChild(document.createElement('div'))
 );
-
-const analytics = new Analytics('UA-109903721-1');
-
-analytics
-    .pageview('http://flex2.kirjava.xyz', '/', 'Main View')
-    .then((response) => { }).catch((err) => { });
 
 document.addEventListener('dragover', (e) => {
     e.preventDefault();
