21d1bb538936e05b422c02ca68703ab03d49dd8a
kirjavascript <snkenjoi@gmail.com>
2017-12-06 20:06:40 +0000

    removed google analytics

--- modules/root.js
+++ modules/root.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Layout } from '#components/layout';
-import packageJson from '../package.json';
 
 import './controls/keyboard';
 import './components/import';
-
-import Analytics from 'electron-google-analytics';
-const devMode = /node_modules[\\/]electron[\\/]/.test(process.execPath);
-if (!devMode) {
-    (new Analytics('UA-109903721-1'))
-        .pageview('http://flex2.kirjava.xyz', `/${packageJson.version}`, 'Main View')
-        .then((response) => { }).catch((err) => { });
-}
 
 render(
     <Layout/>,
