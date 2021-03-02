25f36e9b03465ce4debf9de05ef4bb374eefca4f
kirjavascript <snkenjoi@gmail.com>
2021-03-02 21:56:05 +0000

    updated s1 script

--- app/formats/scripts/index.js
+++ app/formats/scripts/index.js
@@ -4,31 +4,16 @@ export { writeBIN } from './write-bin';
 export { writeASM } from './write-asm';
 export { default as runScript } from './run-script';
 
-// load mapping empty frame bug
 // save/load DPLC enable / disable
-// project screen always open
 // save palette
+// replace project screen
+// project screen always open
 // latest projects / ones in the root
-// ensSprite/emptySprite
 // art ffset
 
 // remove @ deprecated
 // optimizations(false);
 // reverse endianness
-
-// ASM parser: just read data, ignore everything else
-// use arcsecond or remove it
-
-// UI
-
-// file menu
-//      ability to specify offset, size & etc
-//      allow editing filename
-//      format:
-//      add conf to project
-// remove acdeditor
-// purple warning: arbitrary code - warning before yes
-// derive UI from script - make dplcs still optional
 
 // other
 
@@ -41,11 +26,5 @@ export { default as runScript } from './run-script';
 // crackers
 // chaotix
 // plane mappings / snap to
-// s2 special stages
 // github issues
 // sonic3player - unsplit - copy the file and change the format
-
-//unsigned int a = 0xABCDEF23;
-// a = ((a&(0x0000FFFF)) << 16) | ((a&(0xFFFF0000)) >> 16);
-// a = ((a&(0x00FF00FF)) << 8) | ((a&(0xFF00FF00)) >>8);
-// printf("%0x\n",a);
--- scripts/Sonic 1.js
+++ scripts/Sonic 1.js
@@ -16,35 +16,37 @@ const {
 mappings([
     offsetTable(dc.w),
     [
-        ({ mapping, ref }, i) => {
-            if (i === 0) {
-                ref.quantity = read(dc.b);
-                if (ref.quantity === 0) return skipFrame;
-            }
-            mapping.top = read(dc.b, signed);
-            read(nybble);
-            mapping.width = read(2) + 1;
-            mapping.height = read(2) + 1;
-            mapping.priority = read(1);
-            mapping.palette = read(2);
-            mapping.vflip = read(1);
-            mapping.hflip = read(1);
-            mapping.art = read(11);
-            mapping.left = read(dc.b, signed);
-            if (i === ref.quantity - 1) return endFrame;
+        () => {
+            const quantity = read(dc.b);
+            if (quantity === 0) return;
+            return ({ mapping }, i) => {
+                mapping.top = read(dc.b, signed);
+                read(nybble);
+                mapping.width = read(2) + 1;
+                mapping.height = read(2) + 1;
+                mapping.priority = read(1);
+                mapping.palette = read(2);
+                mapping.vflip = read(1);
+                mapping.hflip = read(1);
+                mapping.art = read(11);
+                mapping.left = read(dc.b, signed);
+                if (i === quantity - 1) return endFrame;
+            };
         },
-        ({ mapping, sprite }, i) => {
-            if (i === 0) write(dc.b, sprite.length);
-            write(dc.b, mapping.top);
-            write(nybble, 0);
-            write(2, mapping.width - 1);
-            write(2, mapping.height - 1);
-            write(1, mapping.priority);
-            write(2, mapping.palette);
-            write(1, mapping.vflip);
-            write(1, mapping.hflip);
-            write(11, mapping.art);
-            write(dc.b, mapping.left);
+        ({ sprite }) => {
+            write(dc.w, sprite.length);
+            return ({ mapping }) => {
+                write(dc.b, mapping.top);
+                write(nybble, 0);
+                write(2, mapping.width - 1);
+                write(2, mapping.height - 1);
+                write(1, mapping.priority);
+                write(2, mapping.palette);
+                write(1, mapping.vflip);
+                write(1, mapping.hflip);
+                write(11, mapping.art);
+                write(dc.b, mapping.left);
+            };
         },
     ],
 ]);
@@ -52,19 +54,21 @@ mappings([
 dplcs([
     offsetTable(dc.w),
     [
-        ({ mapping, ref }, i) => {
-            if (i === 0) {
-                ref.quantity = read(dc.b);
-                if (ref.quantity === 0) return skipFrame;
-            }
-            mapping.size = read(nybble) + 1;
-            mapping.art = read(nybble * 3);
-            if (i === ref.quantity - 1) return endFrame;
+        () => {
+            const quantity = read(dc.b);
+            if (quantity === 0) return;
+            return ({ mapping }, frameIndex) => {
+                mapping.size = read(nybble) + 1;
+                mapping.art = read(nybble * 3);
+                if (frameIndex === quantity - 1) return endFrame;
+            };
         },
-        ({ mapping, sprite }, i) => {
-            if (i === 0) write(dc.b, sprite.length);
-            write(nybble, mapping.size);
-            write(nybblr * 3, mapping.art);
+        ({ sprite }) => {
+            write(dc.b, sprite.length);
+            return ({ mapping }) => {
+                write(nybble, mapping.size - 1);
+                write(nybble * 3, mapping.art);
+            };
         },
     ],
 ]);
