2ca71624932887b6a4ba976cdd3d43df8016594d
kirjavascript <snkenjoi@gmail.com>
2026-07-17 18:55:55 +0100

    fix error message

--- app/components/file/error.js
+++ app/components/file/error.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Item } from '#ui';
 
+const MAX_LINES = 8;
+
 function ErrorMsg({ error }) {
+    const [expanded, setExpanded] = useState(false);
+
+    useEffect(() => { setExpanded(false); }, [error]);
+
+    if (!error) return null;
+
+    const lines = error.message.split('\n');
+    const truncated = lines.length > MAX_LINES && !expanded;
+    const visibleLines = truncated ? lines.slice(0, MAX_LINES) : lines;
+
     return (
-        !!error && (
-            <div className="menu-item">
-                <Item color="red">
-                    {error.name}:{' '}
-                    {error.message
-                        .split('\n')
-                        .reduce((acc, cur, i) => [...acc, <br key={i} />, cur])}
-                    <br />
-                </Item>
-            </div>
-        )
+        <div className="menu-item">
+            <Item color="red">
+                {error.name}:{' '}
+                {visibleLines
+                    .reduce((acc, cur, i) => [...acc, <br key={i} />, cur])}
+                <br />
+                {truncated && (
+                    <a
+                        style={{ cursor: 'pointer', textDecoration: 'underline' }}
+                        onClick={() => setExpanded(true)}
+                    >
+                        ... click to show {lines.length - MAX_LINES} more lines
+                    </a>
+                )}
+            </Item>
+        </div>
     );
 }
 
